9cd219f75ba9f61a0233272b1d6627b8ccc334f0
Fabian Schneider <fabian@fabianschneider.org>
2018-06-26 20:55:33 +0200

    Renamed components.

--- Components/TabbarNavigationComponent.tsx
+++ Components/TabbarNavigationComponent.tsx
@@ -1,13 +1,22 @@
-import { TabBarBottom, TabNavigator, createTabNavigator, createBottomTabNavigator } from 'react-navigation';
-import { Settings } from './Settings';
-import { CustomerOverview } from './CustomerOverview';
+import { createBottomTabNavigator } from 'react-navigation';
+import { Entypo } from 'react-native-vector-icons/Entypo'
+import { SettingsComponent } from './SettingsComponent';
+import { CustomerSearchComponent } from './CustomerSearchComponent';
 
 export const TabbarNavigationComponent = createBottomTabNavigator (
     {
-        Customer: { screen: CustomerOverview },
-        Settings: { screen: Settings }
+        Customer: { screen: CustomerSearchComponent },
+        Settings: { screen: SettingsComponent }
     },
     {
+        navigationOptions: ({navigation}) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                // const { routeName } = navigation.state;
+                // if( routeName ==== 'Customer' ) {
+                //     return <Entypo name={'cog'} size={25} color={}/>;
+                // } else 
+            };
+        }),
         tabBarPosition: 'bottom',
         animationEnabled: false,
         swipeEnabled: true
--- README.md
+++ README.md
@@ -8,11 +8,19 @@ React Native TypeScript Proof-of-Concept
 
 * [Microsoft TypeScript Guide](https://github.com/Microsoft/TypeScript-React-Native-Starter)
 
+### Navigation
+* [React Docs](https://reactnavigation.org/docs/en/tab-based-navigation.html)
+* [React-Navigation](https://codeburst.io/ricky-figures-it-out-simple-react-native-tabnavigator-using-react-navigation-592945a3b211)
+
+### Icons
+* [react-native-vector-icons](https://oblador.github.io/react-native-vector-icons/)
+
 ## Features
 * Camera access
 * Local SQLite database
 * REST service calls
 * iOS support
 * UWP support
+* Translations
 
 ## Third Party Libs / Components
