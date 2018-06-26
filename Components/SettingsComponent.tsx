a545986872a6c12c0cdb55a3e1d6eb7bbdfbde9b
Fabian Schneider <fabian@fabianschneider.org>
2018-06-26 20:57:25 +0200

    Readded components again

---
+++ Components/SettingsComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export class SettingsComponent extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Settings</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
